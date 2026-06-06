427ac9c790e04a5335a1fa164278c864be08cef4
benoitlub <benoitlubert@gmail.com>
2026-06-06 13:17:17 +0200

    Restore minimal buildable Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,9 @@
-import { useEffect, useRef, useState } from "react";
 import { useAudioAnalysis } from "../hooks/useAudioAnalysis";
-import { useSpectralBeeps } from "../hooks/useSpectralBeeps";
-import { ParticleField } from "../components/ParticleField";
 import { MicButton } from "../components/MicButton";
 import { TranslationCard } from "../components/TranslationCard";
-import { IntroOverlay } from "../components/IntroOverlay";
 import { SensorScreensV3 } from "../components/SensorScreensV3";
-import { SpectralJournal } from "../components/SpectralJournal";
-import { UI_LABELS, type Lang } from "../data/animals";
-import { createSpectralJournalEntry, saveSpectralJournalEntry, type SpectralJournalEntry } from "../utils/spectralJournal";
 import { SpeciesPanel, EmotionalPanel, ThreatPanel, BiologicalPanel, NeuralPanel, EnvironmentPanel } from "../components/AnalysisPanels";
+import { UI_LABELS } from "../data/animals";
 
-function getSignalPercent(audio
+function getSignalPercent(audioFeatures: any, progress: number) {
+  const rawSignal = Math
