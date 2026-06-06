734f064129f76c21c721d43a4b1af5ad14222c3f
benoitlub <benoitlubert@gmail.com>
2026-06-06 10:23:04 +0200

    Restore stable SpecTRL home layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,22 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAudioAnalysis } from "../hooks/useAudioAnalysis";
 import { useSpectralBeeps } from "../hooks/useSpectralBeeps";
 import { ParticleField } from "../components/ParticleField";
 import { MicButton } from "../components/MicButton";
 import { TranslationCard } from "../components/TranslationCard";
-import { IntroOverlay } from "../components/IntroOverlay"
+import { IntroOverlay } from "../components/IntroOverlay";
+import { SensorScreensV3 } from "../components/SensorScreensV3";
+import { SpectralJournal } from "../components/SpectralJournal";
+import { UI_LABELS, type Lang } from "../data/animals";
+import { createSpectralJournalEntry, saveSpectralJournalEntry, type SpectralJournalEntry } from "../utils/spectralJournal";
+import {
+  SpeciesPanel,
+  EmotionalPanel,
+  ThreatPanel,
+  BiologicalPanel,
+  NeuralPanel,
+  EnvironmentPanel,
+} from "../components/AnalysisPanels";
+
+function getSignalPercent(audioFeatures: any, progress: number) {
+  const rawSignal = Math.round((audioFeatures?.
