1cc9931663f327eeb4d00a9ae805c1f544ac9a3a
benoitlub <benoitlubert@gmail.com>
2026-06-10 16:26:57 +0200

    Load SpecTRL v1.3 safe visual layer

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "./spectrl-overrides.css";
+import "./spectrl-v13-safe.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
